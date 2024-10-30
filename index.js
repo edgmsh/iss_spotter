0166ba4dc7ef8de3d689482873a7239706c2faaa
edgmsh <edgmsh@gmail.com>
2024-10-30 08:28:51 -0400

    function fetchISSFlyOverTimes has been implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 let myIP = "";
 const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
@@ -32,5 +33,22 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   }
 
   console.log('It worked! Returned flyover times:' , passTimes);
+});
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
 
